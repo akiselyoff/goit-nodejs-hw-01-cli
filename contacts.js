61cd0dc0fcfa105f98976ab58e6144a2d2c98e2e
kesel <a.kiselyoff@gmail.com>
2022-08-04 17:48:01 +0300

    refactor contacts functions

--- contacts.js
+++ contacts.js
@@ -3,19 +3,11 @@ const path = require('path');
 
 const contactsPath = path.resolve('./db/contacts.json');
 
-// TODO: задокументировать каждую функцию
 async function listContacts() {
   try {
     const data = await fs.readFile(contactsPath, 'utf-8');
     const parsedData = JSON.parse(data);
-    parsedData.map(contact => {
-      return {
-        id: contact.id,
-        name: contact.name,
-        email: contact.email,
-        phone: contact.phone,
-      };
-    });
+    return parsedData;
   } catch (error) {
     console.log(error.message);
   }
@@ -23,11 +15,11 @@ async function listContacts() {
 
 async function getContactById(contactId) {
   try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const parsedData = JSON.parse(data);
-    const findedContact = parsedData.find(contact => {
-      return contact.id === contactId.toString();
-    });
+    const getListContacts = await listContacts();
+
+    const findedContact = getListContacts.find(
+      contact => contact.id === contactId.toString()
+    );
 
     if (!findedContact) {
       console.log(`there is no contact with id: ${contactId}`);
@@ -40,8 +32,22 @@ async function getContactById(contactId) {
   }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  try {
+    const getListContacts = listContacts();
+    const filteredContacts = getListContacts.filter(
+      contact => contact.id !== contactId.toString()
+    );
+    // if (filteredContacts.length === parsedData.length) {
+    //   console.log('Remove nothing!');
+    //   return;
+    // }
+    const stringifyContacts = JSON.stringify(filteredContacts);
+    await fs.writeFile(contactsPath, stringifyContacts);
+    // return filteredContacts;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 function addContact(name, email, phone) {
