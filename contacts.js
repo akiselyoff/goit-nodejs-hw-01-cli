22517c4ade02fa57491a67c27f60f6232f8b3f61
kesel <a.kiselyoff@gmail.com>
2022-08-04 23:20:25 +0300

    feach: step 4 done, fix: contacts functions refactor

--- contacts.js
+++ contacts.js
@@ -1,57 +1,55 @@
 const fs = require('fs').promises;
 const path = require('path');
+const { nanoid } = require('nanoid');
 
 const contactsPath = path.resolve('./db/contacts.json');
 
 async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const parsedData = JSON.parse(data);
-    return parsedData;
-  } catch (error) {
-    console.log(error.message);
-  }
+  const data = await fs.readFile(contactsPath, 'utf-8');
+  const parsedData = JSON.parse(data);
+
+  return parsedData;
 }
 
 async function getContactById(contactId) {
-  try {
-    const getListContacts = await listContacts();
+  const contacts = await listContacts();
 
-    const findedContact = getListContacts.find(
-      contact => contact.id === contactId.toString()
-    );
+  const findedContact = contacts.find(
+    contact => contact.id === contactId.toString()
+  );
 
-    if (!findedContact) {
-      console.log(`there is no contact with id: ${contactId}`);
-      return;
-    }
-
-    return findedContact;
-  } catch (error) {
-    console.log(error.message);
+  if (!findedContact) {
+    console.log(`there is no contact with id: ${contactId}`);
+    return;
   }
+
+  return findedContact || null;
 }
 
 async function removeContact(contactId) {
-  try {
-    const getListContacts = listContacts();
-    const filteredContacts = getListContacts.filter(
-      contact => contact.id !== contactId.toString()
-    );
-    // if (filteredContacts.length === parsedData.length) {
-    //   console.log('Remove nothing!');
-    //   return;
-    // }
-    const stringifyContacts = JSON.stringify(filteredContacts);
-    await fs.writeFile(contactsPath, stringifyContacts);
-    // return filteredContacts;
-  } catch (error) {
-    console.log(error.message);
+  const contacts = await listContacts();
+
+  const idx = contacts.findIndex(contact => contact.id === contactId);
+  if (idx === -1) {
+    return null;
   }
+  const [deletedContact] = contacts.splice(idx, 1);
+
+  const stringifyContacts = JSON.stringify(contacts, null, 2);
+  await fs.writeFile(contactsPath, stringifyContacts);
+
+  return deletedContact;
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const id = nanoid(5);
+  const newContacts = JSON.stringify(
+    [...contacts, { id, name, email, phone }],
+    null,
+    2
+  );
+  await fs.writeFile(contactsPath, newContacts);
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-const contactsOperations = require('./contacts');
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require('./contacts');
 
-const { listContacts } = contactsOperations;
-const { getContactById } = contactsOperations;
-// console.log(listContacts());
-// console.log(getContactById(11));
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case 'get':
+      const contactById = await getContactById(id);
+      console.log(`Found contact by ${id}: `, contactById);
+      break;
+
+    case 'add':
+      await addContact(name, email, phone);
+      console.log(`You added contact: ${name}, ${email}, ${phone}`);
+      break;
+
+    case 'remove':
+      await removeContact(id);
+      console.log(`Contact with id: ${id} was removed from contact list`);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
+
+// listContacts().then(result => console.log(result));
+// getContactById(8).then(result => console.log(result));
+// removeContact(5);
+// addContact('John Doe', 'john@mail.com', '068-809-50-36');
